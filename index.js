9c981d53157ed78e4ff1e299be925611e3f3deb2
wood-crafter <phanhung2702@gmail.com>
2021-05-20 17:05:35 +0700

    [Add] add song to playlists

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ document.querySelector(`#upload`).addEventListener('submit', e => {
 
   const songName = document.querySelector('#songName').value
   const fileUpload = document.querySelector(`#fileUpload`).files[0]
-
+  //TODO: filter file
   let formData = new FormData()
   formData.append("songName", songName)
   formData.append("song", fileUpload)
@@ -47,23 +47,135 @@ function renderPage() {
     }
   }).then(files => {
     files.forEach(filename => {
-      const a = document.createElement('a')
-      appendSongs(filename, a)
-      addEventSong(filename, a)
+      const div = document.createElement('div')
+      appendSongs(filename, div)
+      appendButton(filename, div)
+
+      document.querySelector("#songs").appendChild(div)
     })
   })
 }
 
-function appendSongs(filename, a) {
+function appendSongs(filename, div) {
+  const a = document.createElement('a')
+
   const fileUrl = `${API_ROOT}/public/${filename}`
+  apiFetch(`/public/songName/${filename}`)
+    .then(result => {
+      return result.text()
+    })
+    .then(result => {
+      a.href = fileUrl
+      a.download = filename
+      a.innerText = result
 
-  a.href = fileUrl
-  a.download = filename
-  a.innerText = filename
+      a.style.display = 'block'
 
-  a.style.display = 'block'
+      addEventSong(filename, a)
 
-  document.querySelector("#songs").appendChild(a)
+      div.appendChild(a)
+    })
+}
+
+function renderPlaylist(filename) {
+  const res = apiFetch(`/playlist_getAll`)
+
+  res.then(res => {
+    if (res.ok) {
+      return res.json()
+    }
+  }).then(playlists => {
+
+    while (document.querySelector("#playlist").firstChild) {
+      document.querySelector("#playlist").firstChild.remove()
+    }
+
+    playlists.forEach(playlist => {
+      appendPlaylist(playlist, filename)
+    })
+
+    appendAddPlaylist(filename)
+  })
+}
+
+function appendAddPlaylist(filename) {
+  const div = document.createElement('div')
+  const songId = filename.substring(0, filename.indexOf('.'))
+  const button = document.createElement('BUTTON')
+
+  button.addEventListener('click', () => {
+    const enterName = document.createElement("INPUT");
+    enterName.setAttribute("type", "text")
+
+    div.appendChild(enterName)
+    addCreatePlaylist(div, enterName, songId)
+  })
+
+  div.appendChild(button)
+  document.querySelector(`#playlist`).appendChild(div)
+}
+
+function addCreatePlaylist(div, input, songId) {
+  const create = document.createElement('BUTTON')
+
+  create.addEventListener('click', () => {
+    if (input.value) {
+      const body = { title: input.value, songId: songId }
+      apiFetch(`/playlist/thisSong`, 'POST', JSON.stringify(body))
+        .then(result => {
+          console.info('OK!')
+        })
+    } else {
+      console.info('Name!')
+    }
+  })
+
+  div.appendChild(create)
+}
+
+function appendPlaylist(playlist, filename) {
+  const songId = filename.substring(0, filename.indexOf('.'))
+  const div = document.createElement('div')
+  div.innerHTML = playlist.title
+  const checkbox = document.createElement("INPUT")
+  checkbox.setAttribute("type", "checkbox")
+  //Check if playlist contain this song
+  const body = {songId: songId, playlistId: playlist.id}
+  apiFetch(`/playlist_containSong`, 'POST', JSON.stringify(body))
+    .then(res => {
+      return res.json()
+    })
+    .then(result => {
+      if (result.length) {
+        checkbox.checked = true
+      }
+    })
+
+  checkbox.addEventListener('change', function () {
+    if (this.checked) {
+      const body = { song_id: songId, playlist_id: playlist.id }
+      console.info(JSON.stringify(body))
+      apiFetch(`/playlist/song`, 'POST', JSON.stringify(body))
+    } else {
+      const body = { song_id: songId, playlist_id: playlist.id }
+      apiFetch(`/playlist/song`, 'DELETE', JSON.stringify(body))
+    }
+  })
+
+  div.appendChild(checkbox)
+  document.querySelector(`#playlist`).appendChild(div)
+}
+
+function appendButton(filename, div) {
+  const button = document.createElement('BUTTON')
+  button.id = filename
+
+  button.addEventListener('click', () => {
+    document.querySelector(`#myModal`).style.display = "block"
+    renderPlaylist(filename)
+  })
+
+  div.appendChild(button)
 }
 
 function addEventSong(filename, a) {
@@ -117,30 +229,48 @@ function getAudio(filename) {
 
 function updatePreview(previewEl) {
   const container = document.querySelector(`#${PREVIEW_CONTAINER_ID}`)
-
-
     ;[...container.children].forEach(child => child.remove())
-  // for (child of container.children) {
-  //   child.remove()
-  // }
 
   container.append(previewEl)
 }
 
 function apiFetch(url, method = 'GET', body) {
+  const fullUrl = `${API_ROOT}${url}`
   if (body) {
-    return fetch(`${API_ROOT}${url}`, {
-      method,
-      headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token') || ''}`
-      },
-      body: body
-    })
+    if (typeof (body) == 'string') {
+      return fetch(fullUrl, {
+        method,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem('token') || ''}`
+        },
+        body: body
+      })
+    }
+    else {
+      return fetch(fullUrl, {
+        method,
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('token') || ''}`
+        },
+        body: body
+      })
+    }
   }
-  return fetch(`${API_ROOT}${url}`, {
+  return fetch(fullUrl, {
     method,
     headers: {
       'Authorization': `Bearer ${localStorage.getItem('token') || ''}`
     }
   })
+}
+
+document.querySelector(`#span`).addEventListener('click', () => {
+  document.querySelector(`#myModal`).style.display = "none"
+})
+
+window.onclick = function (event) {
+  if (event.target == document.querySelector(`#myModal`)) {
+    document.querySelector(`#myModal`).style.display = "none";
+  }
 }
